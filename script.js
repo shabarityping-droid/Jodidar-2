569e2ed83711a093aef5d019b1e31abb56644d45
shabarityping-droid <shabarityping@gmail.com>
2026-07-04 02:36:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-
-// Login तपासा
 const loginBtn = document.getElementById("loginBtn");
 const logoutBtn = document.getElementById("logoutBtn");
 
 if (localStorage.getItem("loggedInUser")) {
-    if (loginBtn) loginBtn.style.display = "none";
-    if (logoutBtn) logoutBtn.style.display = "inline-block";
+  if (loginBtn) loginBtn.style.display = "none";
+  if (logoutBtn) logoutBtn.style.display = "inline-block";
 } else {
-    if (loginBtn) loginBtn.style.display = "inline-block";
-    if (logoutBtn) logoutBtn.style.display = "none";
+  if (loginBtn) loginBtn.style.display = "inline-block";
+  if (logoutBtn) logoutBtn.style.display = "none";
 }
 
-// Logout
 function logout() {
-    localStorage.removeItem("loggedInUser");
-    alert("Logout यशस्वी!");
-    window.location.href = "login.html";
+  localStorage.removeItem("loggedInUser");
+  window.location.href = "login.html";
 }
