4a67130c5dac1dab9aae9902bb46f40167e3ee67
shabarityping-droid <shabarityping@gmail.com>
2026-07-04 21:09:07 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,26 @@
 
+// login.js
+
+import { auth } from "./firebase-config.js";
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+
+    alert("Login Successful!");
+
+    window.location.href = "dashboard.html";
+  } catch (error) {
+    alert(error.message);
+  }
+});
