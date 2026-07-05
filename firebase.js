ac34aa6d6b2e6a777df61aa6cedd36763eb1853f
shabarityping-droid <shabarityping@gmail.com>
2026-07-05 12:54:40 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,23 @@
 // Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// तुमचा Firebase Config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyCKQVsvvx022jItJVLSBpiokzsfSE2v5gM",
   authDomain: "jodidar-3ea53.firebaseapp.com",
   projectId: "jodidar-3ea53",
   storageBucket: "jodidar-3ea53.firebasestorage.app",
   messagingSenderId: "1059717419145",
-  appId: "YOUR_APP_ID"
+  appId: "1:1059717419145:web:ee77586d288c0d2853f658",
+  measurementId: "G-SF5ESND7QX"
 };
 
-// Firebase सुरू करा
 const app = initializeApp(firebaseConfig);
 
-// Firestore
-const db = getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 
-// Export
-export { db };
+export default app;
