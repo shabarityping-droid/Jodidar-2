9f6eccbcfe11bf8f126a4971f3cfb5f18191081b
shabarityping-droid <shabarityping@gmail.com>
2026-07-05 12:26:14 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,57 @@
-const loginForm = document.getElementById("loginForm");
+import { db } from "./firebase.js";
+import {
+  collection,
+  query,
+  where,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-loginForm.addEventListener("submit", function (e) {
-    e.preventDefault();
+window.loginUser = async function () {
 
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
+    const customerId = document.getElementById("customerId").value.trim();
+    const mobile = document.getElementById("mobile").value.trim();
 
-    if (username === "admin" && password === "123456") {
-        alert("Login Successful!");
-        window.location.href = "dashboard.html";
-    } else {
-        alert("Wrong Username or Password!");
+    if (customerId === "" || mobile === "") {
+        alert("Customer ID आणि Mobile Number भरा.");
+        return;
     }
-});
+
+    try {
+
+        const q = query(
+            collection(db, "users"),
+            where("customerId", "==", customerId),
+            where("mobile", "==", mobile)
+        );
+
+        const querySnapshot = await getDocs(q);
+
+        if (querySnapshot.empty) {
+            alert("Customer ID किंवा Mobile Number चुकीचा आहे.");
+            return;
+        }
+
+        const user = querySnapshot.docs[0].data();
+
+        if (user.approved === false) {
+            alert("तुमचे प्रोफाइल Admin Approval साठी प्रलंबित आहे.");
+            return;
+        }
+
+        localStorage.setItem("customerId", user.customerId);
+        localStorage.setItem("mobile", user.mobile);
+        localStorage.setItem("name", user.fullName);
+
+        alert("Login Successful");
+
+        window.location.href = "home.html";
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("Login Failed. पुन्हा प्रयत्न करा.");
+
+    }
+
+};
