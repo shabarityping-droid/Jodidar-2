36d5c94bd289e25978b8e47a37acacf7555b08e0
shabarityping-droid <shabarityping@gmail.com>
2026-07-05 15:09:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,22 @@
-// Firebase SDK
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKQVsvvx022jItJVLSBpiokzsfSE2v5gM",
-  authDomain: "jodidar-3ea53.firebaseapp.com",
-  projectId: "jodidar-3ea53",
-  storageBucket: "jodidar-3ea53.firebasestorage.app",
-  messagingSenderId: "1059717419145",
-  appId: "1:1059717419145:web:ee77586d288c0d2853f658",
-  measurementId: "G-SF5ESND7QX"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
 
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
