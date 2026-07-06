d0e9148d130ab75dd065fd16c4599d89af874000
shabarityping-droid <shabarityping@gmail.com>
2026-07-06 16:42:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,36 +1,27 @@
-// Firebase SDK
+// Firebase SDK Import
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  doc,
-  updateDoc,
-  deleteDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// तुमचा Firebase Config येथे ठेवा
+// Firebase Config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCKQVsvvx022jItJVLSBpiokzsfSE2v5gM",
+  authDomain: "jodidar-3ea53.firebaseapp.com",
+  databaseURL: "https://jodidar-3ea53-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "jodidar-3ea53",
+  storageBucket: "jodidar-3ea53.firebasestorage.app",
+  messagingSenderId: "1059717419145",
+  appId: "1:1059717419145:web:ee77586d288c0d2853f658",
+  measurementId: "G-SF5ESND7QX"
 };
 
-// Firebase Initialize
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Firestore
 const db = getFirestore(app);
 
-// Export
-export {
-  db,
-  collection,
-  addDoc,
-  getDocs,
-  doc,
-  updateDoc,
-  deleteDoc
-};
+// Storage
+const storage = getStorage(app);
+
+export { app, db, storage };
