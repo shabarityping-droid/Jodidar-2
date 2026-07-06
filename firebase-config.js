14a0fd27002b982799c02c8b3ee58b00eb90ba9b
shabarityping-droid <shabarityping@gmail.com>
2026-07-06 19:54:26 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,19 @@
-// firebase-config.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCKQVsvvx022jItJVLSBpiokzsfSE2v5gM",
+  authDomain: "jodidar-3ea53.firebaseapp.com",
+  databaseURL: "https://jodidar-3ea53-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "jodidar-3ea53",
+  storageBucket: "jodidar-3ea53.firebasestorage.app",
+  messagingSenderId: "1059717419145",
+  appId: "1:1059717419145:web:ee77586d288c0d2853f658",
+  measurementId: "G-SF5ESND7QX"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getDatabase(app);
 const storage = getStorage(app);
 
