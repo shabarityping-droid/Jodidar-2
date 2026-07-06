e4db0d4e0adc55f9c258ea259a5f8af64b8d092e
shabarityping-droid <shabarityping@gmail.com>
2026-07-06 14:01:04 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,1 +1,18 @@
 
+import { auth } from "./firebase.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        window.location.href = "login.html";
+    }
+});
+
+const button = document.querySelector(".logout");
+
+button.addEventListener("click", () => {
+    signOut(auth).then(() => {
+        alert("Logout Successful");
+        window.location.href = "login.html";
+    });
+});
