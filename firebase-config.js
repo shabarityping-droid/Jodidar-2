6efd29c2d7f673c2c36c9aaac1835620fc253228
shabarityping-droid <shabarityping@gmail.com>
2026-07-06 17:48:56 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,25 @@
-// firebase-config.js
-
+// Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
+// Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCKVQsvxv02ZjItJVLSBp10ixsrfSE2v5gM",
+  apiKey: "AIzaSyCKQVsvvx022jItJVLSBpiokzsfSE2v5gM",
   authDomain: "jodidar-3ea53.firebaseapp.com",
+  databaseURL: "https://jodidar-3ea53-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "jodidar-3ea53",
   storageBucket: "jodidar-3ea53.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  messagingSenderId: "165551441273",
+  appId: "1:165551441273:web:YOUR_APP_ID"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { app, auth, db };
+// Export Services
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
+export default app;
