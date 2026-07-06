ac5c32e87bb759d00f5b3c38bdca7c0d4c30f2a2
shabarityping-droid <shabarityping@gmail.com>
2026-07-06 11:24:48 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,12 @@
 // Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-import {
-  getStorage
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCKQVsvvx022jItJVLSBpiokzsfSE2v5gM",
+  apiKey: "AIzaSyCKQVSvxx022jItJVLSBpiokzsfSE2v5gM",
   authDomain: "jodidar-3ea53.firebaseapp.com",
   projectId: "jodidar-3ea53",
   storageBucket: "jodidar-3ea53.firebasestorage.app",
@@ -25,9 +17,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Services
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+// Services
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-export default app;
+// Export
+export { auth, db, storage };
