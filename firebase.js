34bfe869e4960e1d21fe32faad0c205c6b2cf628
shabarityping-droid <shabarityping@gmail.com>
2026-07-09 23:16:11 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,17 @@
 // firebase.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "तुमची apiKey",
-  authDomain: "तुमचा-project.firebaseapp.com",
-  projectId: "तुमचा-project-id",
-  storageBucket: "तुमचा-project.appspot.com",
-  messagingSenderId: "तुमचा Sender ID",
+  apiKey: "AIzaSyCKQVsvvx022jItJVLSBpiokzsfSE2v5gM",
+  authDomain: "jodidar-3ea53.firebaseapp.com",
+  databaseURL: "https://jodidar-3ea53-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "jodidar-3ea53",
+  storageBucket: "jodidar-3ea53.firebasestorage.app",
+  messagingSenderId: "449829906613",
   appId: "तुमचा App ID"
 };
 
