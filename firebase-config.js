27581479d544a213c618dd081d8a571278096447
shabarityping-droid <shabarityping@gmail.com>
2026-07-09 23:22:36 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKQVsvvx022jItJVLSBpiokzsfSE2v5gM",
-  authDomain: "jodidar-3ea53.firebaseapp.com",
-  databaseURL: "https://jodidar-3ea53-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "jodidar-3ea53",
-  storageBucket: "jodidar-3ea53.firebasestorage.app",
-  messagingSenderId: "1059717419145",
-  appId: "1:1059717419145:web:d44b6a53c8d4458853f658"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getDatabase(app);
+export { app };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
