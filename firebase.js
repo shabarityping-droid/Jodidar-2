bf0e65b6236cad33f659278ddb5675228a977983
shabarityping-droid <shabarityping@gmail.com>
2026-07-09 23:27:13 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,8 @@
-// firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCKQVsvvx022jItJVLSBpiokzsfSE2v5gM",
@@ -11,8 +10,9 @@ const firebaseConfig = {
   databaseURL: "https://jodidar-3ea53-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "jodidar-3ea53",
   storageBucket: "jodidar-3ea53.firebasestorage.app",
-  messagingSenderId: "449829906613",
-  appId: "तुमचा App ID"
+  messagingSenderId: "1059717419145",
+  appId: "1:1059717419145:web:ee77586d288c0d2853f658",
+  measurementId: "G-SF5ESND7QX"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -20,5 +20,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const rtdb = getDatabase(app);
 
-export { app, auth, db, storage };
+export { app, auth, db, storage, rtdb };
