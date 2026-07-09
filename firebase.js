f0607f1f7a4cac8fb929174b596cedca4b2f097f
shabarityping-droid <shabarityping@gmail.com>
2026-07-09 22:42:07 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCKQVsvvx022jItJVLSBpiokzsfSE2v5gM",
-  authDomain: "jodidar-3ea53.firebaseapp.com",
-  databaseURL: "https://jodidar-3ea53-default-rtdb.asia-southeast1.firebasedatabase.app",
+  apiKey: "...",
+  authDomain: "...",
   projectId: "jodidar-3ea53",
   storageBucket: "jodidar-3ea53.firebasestorage.app",
-  messagingSenderId: "1059717419145",
-  appId: "1:1059717419145:web:ee77586d288c0d2853f658",
-  measurementId: "G-SF5ESND7QX"
+  messagingSenderId: "...",
+  appId: "..."
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const database = getDatabase(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
