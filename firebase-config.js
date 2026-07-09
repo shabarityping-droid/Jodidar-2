9e7c8e09d5a400c90f4488af6fa8ee6565499742
shabarityping-droid <shabarityping@gmail.com>
2026-07-09 07:59:21 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
+// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyCKQVsvvx022jItJVLSBpiokzsfSE2v5gM",
   authDomain: "jodidar-3ea53.firebaseapp.com",
@@ -13,9 +15,12 @@ const firebaseConfig = {
   measurementId: "G-D7ZE8FLWC5"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const db = getDatabase(app);
-const storage = getStorage(app);
+// Export Services
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
 
-export { app, db, storage };
+export default app;
